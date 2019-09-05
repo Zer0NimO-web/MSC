83b90604d40107ca5b2acf361fe5e2ace3a6c402
Zer0Nim0-web <olivier.berton133@gmail.com>
2019-09-05 10:56:18 +0200

    Initialisation du projet  - MSC

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController{
+
+	/**
+	 * @Route ("/", name="home.index")
+	 * @return Response
+	 */
+	public function index(): Response
+	{
+		return $this->render('home/home.html.twig', [
+			'current_menu' => 'home'
+		]);
+	}
+
+}
